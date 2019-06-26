f6309ba967b0f708d0ee6f01559dd5d20c4908a4
we <we@DESKTOP-T0NVBPN>
2019-06-26 18:28:00 +0300

    Add project files.

---
+++ my project hhh/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_project_hhh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ali = 5;
+            Console.WriteLine(ali);
+        }
+    }
+}
